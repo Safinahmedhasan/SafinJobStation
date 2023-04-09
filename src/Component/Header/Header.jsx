d54dc68f7dacdb2f332a3e41ad590126bd3780f0
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-04-10 00:38:34 +0600

    first commit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h2></h2>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Header;
