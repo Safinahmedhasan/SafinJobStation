6e8a482ae6247909f04d410a0c0f748072344bf0
safinahmedhasan <safinahmedhaasan@gmail.com>
2023-04-11 15:13:56 +0600

    Job Details Section Added

--- src/Component/JobDetails/Jobdetails.jsx
+++ src/Component/JobDetails/Jobdetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import JobDetailsExtraPart from '../JobDetailsExtraPart/JobDetailsExtraPart';
 const Jobdetails = () => {
 
     const [jobs, setJobs] = useState([]);
@@ -17,12 +18,54 @@ const Jobdetails = () => {
     const storeJobData = jobsSingleData.filter((job) => job.id === jobId);
 
 
-    const { id , title} = storeJobData[0]
+    const { title, salary, location, jobResponsibilities, educationalRequirements, experiences, phone, Email, jobDescription } = storeJobData[0]
 
     return (
         <div>
-            <h2>{id}</h2>
-            <h2>{title}</h2>
+            <div className='bg-slate-100 relative h-64'>
+                <h2 className='font-extrabold text-3xl text-[#1A1919] pt-20 text-center'>Job Details</h2>
+                <img className=' absolute ml-20 mt-[-80px]' src="/src/assets/All Images/Vector.png" alt="" />
+                <img className=' absolute ml-[1100px] z-10 mt-[-250px] ' src="/src/assets/All Images/Vector-1.png" alt="" />
+            </div>
+
+
+            <div className='container mx-auto mt-20 mb-20 grid md:grid-cols-3'>
+                <div className='p-20 md:col-span-2'>
+                    <h2><span className='mt-2 font-semibold'>Job Description : </span>{jobDescription}</h2>
+                    <h2 className='mt-4'><span className='mt-2 font-semibold'> Job Responsibility :</span> {jobResponsibilities}</h2>
+                    <h2 className='mt-4'><span className='mt-2 font-semibold'>Educational Requirements :</span> {educationalRequirements}</h2>
+                    <h2 className='mt-4'><span className='mt-2 font-semibold'>Experiences :</span> {experiences}</h2>
+                </div>
+
+                <div>
+
+                <div className=' bg-slate-100 w-96 h-96 p-5  rounded-lg container mx-auto'>
+                    <div className=''>
+                        <h2 className='font-semibold mt-5 mb-3 text-2xl'>Job Details</h2>
+                        <hr></hr>
+                        
+                        <p className='mt-3'> <span className='mt-2 font-semibold'>Salary  : </span>{salary}</p>
+                        <p > <span className='mt-2 font-semibold'>Job Title : </span>{title}</p>
+                    </div>
+
+                    <div className=' md:col-span-1'>
+                        <h2 className='font-semibold text-2xl mt-5 mb-3'>Contact Information</h2>
+                        <hr></hr>
+
+                        <p className='mt-3'><span className='mt-2 font-semibold'>Phone : </span>{phone}</p>
+                        <p><span className='mt-2 font-semibold'>Email : </span>{Email}</p>
+                        <p> <span className='mt-2 font-semibold'>Phone : </span>{location}</p>
+                    </div>
+                    <button className='text-white mt-24 bg-gradient-to-r from-indigo-600 to-blue-500 p-3 rounded w-full text-center my-2' >Apply Now</button>
+                    
+                </div>
+
+                </div>
+
+            </div>
+
+
+
         </div>
     );
 };
---
+++ src/Component/JobDetailsExtraPart/JobDetailsExtraPart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobDetailsExtraPart = () => {
+    return (
+        <div>
+            <h2>This is Now Extra part</h2>
+        </div>
+    );
+};
+
+export default JobDetailsExtraPart;
